1871445f1da38f746d646c4272806651705d7327
Artem Razin <razin@softanics.com>
2021-03-23 11:19:12 +0300

    Added the project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace armdot_tutorial_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCheckPassword = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCheckLicense = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCheckPassword
+            // 
+            this.buttonCheckPassword.Location = new System.Drawing.Point(12, 102);
+            this.buttonCheckPassword.Name = "buttonCheckPassword";
+            this.buttonCheckPassword.Size = new System.Drawing.Size(158, 23);
+            this.buttonCheckPassword.TabIndex = 0;
+            this.buttonCheckPassword.Text = "Check Password";
+            this.buttonCheckPassword.UseVisualStyleBackColor = true;
+            this.buttonCheckPassword.Click += new System.EventHandler(this.buttonCheckPassword_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(105, 13);
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(224, 73);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Password: ";
+            // 
+            // buttonCheckLicense
+            // 
+            this.buttonCheckLicense.Location = new System.Drawing.Point(174, 102);
+            this.buttonCheckLicense.Name = "buttonCheckLicense";
+            this.buttonCheckLicense.Size = new System.Drawing.Size(155, 23);
+            this.buttonCheckLicense.TabIndex = 3;
+            this.buttonCheckLicense.Text = "Check License";
+            this.buttonCheckLicense.UseVisualStyleBackColor = true;
+            this.buttonCheckLicense.Click += new System.EventHandler(this.buttonCheckLicense_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 159);
+            this.Controls.Add(this.buttonCheckLicense);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.buttonCheckPassword);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCheckPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCheckLicense;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace armdot_tutorial_sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [ArmDot.Client.VirtualizeCode]
+        private void buttonCheckPassword_Click(object sender, EventArgs e)
+        {
+            byte[] hash = (new SHA256Managed()).ComputeHash(Encoding.Unicode.GetBytes(textBoxPassword.Text));
+
+            if (hash[0] == 81 && hash[1] == 96 && hash[2] == 9 && hash[3] == 150 &&
+                hash[4] == 45 && hash[5] == 146 && hash[6] == 51 && hash[7] == 201 &&
+                hash[8] == 238 && hash[9] == 22 && hash[10] == 103 && hash[11] == 233 &&
+                hash[12] == 209 && hash[13] == 135 && hash[14] == 107 && hash[15] == 39 &&
+                hash[16] == 228 && hash[17] == 171 && hash[18] == 22 && hash[19] == 78 &&
+                hash[20] == 218 && hash[21] == 213 && hash[22] == 11 && hash[23] == 131 &&
+                hash[24] == 71 && hash[25] == 17 && hash[26] == 241 && hash[27] == 180 &&
+                hash[28] == 118 && hash[29] == 9 && hash[30] == 163 && hash[31] == 249)
+            {
+                MessageBox.Show("The password is correct");
+            }
+            else
+            {
+                MessageBox.Show("The password is wrong");
+            }
+        }
+
+        [ArmDot.Client.VirtualizeCode]
+        private void buttonCheckLicense_Click(object sender, EventArgs e)
+        {
+            ArmDot.Client.Api.PutKey(textBoxPassword.Text);
+
+            string state;
+            string userName = null;
+
+            switch (ArmDot.Client.Api.GetLicenseState())
+            {
+                case ArmDot.Client.Api.LicenseKeyState.Valid:
+                    {
+                        state = "valid";
+                        userName = ArmDot.Client.Api.GetUserName();
+                        break;
+                    }
+                case ArmDot.Client.Api.LicenseKeyState.Blocked:
+                    {
+                        state = "blocked";
+                        break;
+                    }
+                case ArmDot.Client.Api.LicenseKeyState.Expired:
+                    {
+                        state = "expired";
+                        break;
+                    }
+                case ArmDot.Client.Api.LicenseKeyState.MaximumBuildDateExpired:
+                    {
+                        state = "maximum build date expired";
+                        break;
+                    }
+                case ArmDot.Client.Api.LicenseKeyState.BadHardwareId:
+                    {
+                        state = "bad hardware id";
+                        break;
+                    }
+                case ArmDot.Client.Api.LicenseKeyState.Invalid:
+                default:
+                    {
+                        state = "invalid";
+                        break;
+                    }
+            }
+
+            MessageBox.Show($"The key state is {state}, user name is {userName ?? "<unknown>"}");
+        }
+    }
+}
